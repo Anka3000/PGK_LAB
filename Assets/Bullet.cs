efbcadf193ce9abd9f927c714eaaccc468a67be7
Ann <94915856+Anka3000@users.noreply.github.com>
2024-03-27 13:10:32 +0100

    remove_obj
    kwadrat strzela kulkami i niszczy kostki

---
+++ Assets/Bullet.cs
@@ -0,0 +1,25 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+        public float life = 1;
+
+        void Awake()
+        {
+            Destroy(gameObject, life);
+        }
+
+        void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("Player")) 
+            {
+                return; 
+            }
+         Destroy(collision.gameObject);
+         Destroy(gameObject);
+
+        }
+}
---
+++ Assets/Gun.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Gun : MonoBehaviour
+{
+    public Transform bulletSpawnPoint;
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 10.0f;
+
+    void Update()
+    {
+        
+    }
+
+    public void Fire(InputAction.CallbackContext context)
+    {
+        var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+    }
+}
